e822020dbeb3a84869f5369cc269301458c67ba1
Fernando Ayats <ayatsfer@gmail.com>
2023-10-09 18:56:21 +0200

    eval id

--- src/eval.rs
+++ src/eval.rs
@@ -4,19 +4,30 @@ use tracing_test::traced_test;
 
 use crate::{Ast, FunctionCall, Int, Literal};
 use core::panic;
-use std::{collections::HashMap, error::Error, fmt::Display, marker::PhantomData, sync::Arc};
+use std::ops::Deref;
+use std::{
+    collections::HashMap, error::Error, fmt::Display, marker::PhantomData, rc::Rc, sync::Arc,
+};
 
 use crate::Expression;
 
 #[derive(Debug, Clone)]
-struct Object(Arc<ObjectRaw>);
+struct Object<'o>(Rc<ObjectRaw<'o>>);
 
-impl Object {
+impl<'o> Deref for Object<'o> {
+    type Target = ObjectRaw<'o>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'o> Object<'o> {
     fn new_function<F>(func: F) -> Self
     where
-        F: Fn(Object) -> Object,
+        F: Fn(Object) -> Object + 'o,
     {
-        Object(Arc::new(ObjectRaw::Function(Function {
+        Object(From::from(ObjectRaw::Function(Function {
             value: Box::new(func),
         })))
     }
@@ -25,22 +36,22 @@ impl Object {
     where
         I: Into<Int>,
     {
-        Object(Arc::new(ObjectRaw::Int(int.into())))
+        Object(From::from(ObjectRaw::Int(int.into())))
     }
 }
 
 #[derive(Debug)]
-enum ObjectRaw {
+enum ObjectRaw<'o> {
     Int(Int),
     String(String),
-    Function(Function),
+    Function(Function<'o>),
 }
 
 #[derive(Educe)]
 #[educe(Debug)]
-struct Function {
+struct Function<'f> {
     #[educe(Debug(ignore))]
-    value: Box<dyn Fn(Object) -> Object>,
+    value: Box<dyn Fn(Object) -> Object + 'f>,
 }
 
 #[test]
@@ -48,7 +59,7 @@ struct Function {
 fn test_eval<'src>() {
     let ast = Ast::FunctionCall(FunctionCall {
         function: Box::new(Ast::Identifier(crate::Identifier {
-            name: String::from("+1"),
+            name: String::from("id"),
         })),
         argument: Box::new(Ast::Literal(Literal::Integer(1.into()))),
     });
@@ -86,12 +97,12 @@ impl Error for EvalError {}
 type EvalResult<T> = Result<T, EvalError>;
 
 #[derive(Debug)]
-struct Scope<'scope> {
+struct Scope<'scope, 'o> {
     parent: Option<&'scope Self>,
-    bindings: HashMap<String, Object>,
+    bindings: HashMap<String, Object<'o>>,
 }
 
-impl<'scope> Scope<'scope> {
+impl<'scope, 'o> Scope<'scope, 'o> {
     fn symbol_lookup<S: AsRef<str>>(&'scope self, symbol: S) -> Object {
         self.bindings
             .get(&symbol.as_ref().to_string())
@@ -100,7 +111,9 @@ impl<'scope> Scope<'scope> {
     }
 }
 
-fn eval(node: Ast, scope: &Scope) -> EvalResult<Object>
+fn eval<'scope, 'o>(node: Ast, scope: &'scope Scope) -> EvalResult<Object<'o>>
+where
+    'scope: 'o,
 {
     match node {
         Ast::Literal(lit) => match lit {
@@ -108,6 +121,16 @@ fn eval(node: Ast, scope: &Scope) -> EvalResult<Object>
             _ => Err(EvalError::Todo),
         },
         Ast::Identifier(ident) => Ok(scope.symbol_lookup(&ident.name)),
-        _ => Err(EvalError::Todo),
+        Ast::FunctionCall(call) => {
+            let function = eval(*call.function, &scope).unwrap();
+            let argument = eval(*call.argument, &scope).unwrap();
+            match *function {
+                ObjectRaw::Function(ref f) => {
+                    let result = (f.value)(argument);
+                    Ok(result)
+                }
+                _ => Err(EvalError::TypeError),
+            }
+        }
     }
 }
